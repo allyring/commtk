b8ed29fa687a210f61cce63ef732b0592fe15dfc
Name <107352419+allyring@users.noreply.github.com>
2022-12-19 19:34:00 +0000

    Fix loadout tooling

--- cfg/config.go
+++ cfg/config.go
@@ -1,71 +1,133 @@
 package cfg
 
 import (
+	"encoding/json"
+	"errors"
 	jsonschema "github.com/xeipuuv/gojsonschema"
+	"strconv"
+	"time"
 )
 
 var Config = &Cfg{}
 
-type Cfg struct {
-	Layout	bool
+
+type LayoutNode struct {
+	childSizePercentages []int
+	verticalStacked bool
+
+	children []LayoutNode
+
+	tool string
+	address string
+}
+
+type JsonLayoutImport struct {
+	Leaf bool `json:"Leaf"`
+	Tool string `json:"ToolName"`
+
+	ChildOrderedSizePercentages []int `json:"ChildOrderedSizePercentages"`
+	VerticalStacked bool `json:"VerticalStacked"`
+
+	ChildrenParsed []JsonLayoutImport `json:"Children"`
 }
 
 
-func LoadLayoutFile(filepath string) (error, []jsonschema.ResultError) {
-	// Load JSON, then verify it against a schema
-	// Parse it recursively into a node tree.
-	// Return node tree
+type Cfg struct {
+	LayoutRoot LayoutNode
+}
 
-	//var documentLoader jsonschema.JSONLoader
-	//
-	//if filepath == "" {
-	//	documentLoader = jsonschema.NewStringLoader(defaultLayoutConfig)
-	//} else {
-	//	documentLoader = jsonschema.NewReferenceLoader(filepath)
-	//}
-	//
-	//
-	//
-	//schemaLoader := jsonschema.NewStringLoader(layoutJsonSchema)
-	//
-	//result, err := jsonschema.Validate(schemaLoader, documentLoader)
-	//if err != nil {
-	//	// Error while reading schema
-	//	return err, nil
-	//}
-	//
-	//
-	//if result.Valid() {
-	//	// Parse data into variable here
-	//	var fileData []byte
-	//
-	//	if filepath != "" {
-	//		fdCache, err := os.ReadFile(filepath)
-	//
-	//		if err != nil {
-	//			return err, nil
-	//		}
-	//
-	//		fileData = fdCache
-	//	} else {
-	//		fileData = []byte(defaultLayoutConfig)
-	//	}
-	//
-	//
-	//	var cells [][]GridCell
-	//	if err := json.Unmarshal(fileData, &cells); err != nil {
-	//		return err, nil
-	//	}
-	//
-	//	Config.Layout = cells
-	//
-	//
-	//	return nil, nil
-	//} else {
-	//	// Error in config file
-	//	return err, result.Errors()
-	//	}
+const (
+	defaultConfig string = "{'ChildOrderedSizePercentages': [33,67],'Children': [{'Leaf': true,'ToolName': 'T1'},{'Leaf': false,'VerticalStacked': true,'Children': [{'Children': [{'leaf': true,'toolName': 'T3'},{'leaf': true, 'toolName': 'T4'}]},{'leaf': true, 'toolName': 'T2'}]}]}"
+
+)
+
+func ParseJSON(currentJson string) (JsonLayoutImport, error) {
+	parsed := JsonLayoutImport{}
+	err := json.Unmarshal([]byte(currentJson), &parsed)
+
+	return parsed, err
+}
+
+func ParseToLayoutTree(parsedJSON JsonLayoutImport) (LayoutNode, error) {
+	currentNode := LayoutNode{}
+
+	currentNode.childSizePercentages = parsedJSON.ChildOrderedSizePercentages
+	currentNode.verticalStacked = parsedJSON.VerticalStacked
+
+	if (parsedJSON.Leaf) && (len(parsedJSON.ChildrenParsed) > 0) {
+		// Leaf has children, so return an error
+		return currentNode, errors.New("cannot have a tool with children")
+	}
 
 
+	if parsedJSON.Leaf {
+		currentNode.tool = parsedJSON.Tool
+		// Generate address suffix for currentNode based on unix time nanoseconds
+		currentNode.address = parsedJSON.Tool + strconv.FormatInt(time.Now().UnixNano(), 10)
+		return currentNode, nil
+	}
+
+	// Dealing with child nodes
+	for _, child := range parsedJSON.ChildrenParsed {
+		node, err := ParseToLayoutTree(child)
+		if err != nil {
+			return currentNode, err
+		}
+		currentNode.children = append(currentNode.children, node)
+	}
+
+	return currentNode, nil
+}
+
+func LoadLayoutFile(fileData string) (error, []jsonschema.ResultError) {
+	// Load JSON, then verify it against the schema
+
+	var documentLoader jsonschema.JSONLoader
+
+	if fileData == "" {
+		// No config file passed in, so use default layout config
+		documentLoader = jsonschema.NewStringLoader(defaultConfig)
+	} else {
+		documentLoader = jsonschema.NewStringLoader(fileData)
+	}
+
+	// Load JSON schema from file
+	schemaLoader := jsonschema.NewReferenceLoader("file:///mnt/c/Users/Ally%20R/Desktop/Programming/Go/commtk/schema.json")
+
+	// Validate document
+	result, err := jsonschema.Validate(documentLoader, schemaLoader)
+
+	// Return any json validation errors
+	// TODO: Update schema to allow single tool layouts
+	if err != nil {
+		return err, nil
+	}
+
+	if result.Errors() != nil {
+		return nil, result.Errors()
+	}
+
+	// Parse it recursively into a layout tree
+	var jsonData []byte
+	if fileData != "" {
+		jsonData = []byte(fileData)
+	} else {
+		jsonData = []byte(defaultConfig)
+	}
+
+
+	parsedJson, err := ParseJSON(string(jsonData))
+
+	// Debug for JSON parsing
+	// s, _ := json.MarshalIndent(final, "", "\t")
+	// fmt.Println(string(s))
+
+	// Perform some extra parsing on the parsed JSON
+
+	final, err := ParseToLayoutTree(parsedJson)
+
+	Config.LayoutRoot = final
+
+	// Save configNode tree into variable Config
 	return nil, nil
 }
--- main.go
+++ main.go
@@ -16,179 +16,71 @@ package main
 
 import (
 	"fmt"
+	"github.com/allyring/commtk/cfg"
+	"os"
+	"strconv"
 
-	// Bubbletea for the TUI
-	tea "github.com/charmbracelet/bubbletea"
-
-	// Bubbletea bubbles
-	"github.com/charmbracelet/bubbles/spinner"
 	boxer "github.com/treilik/bubbleboxer"
+
 )
 
-const (
-	upperAddr  = "upper"
-	leftAddr   = "left"
-	middleAddr = "middle"
-	rightAddr  = "right"
-	lowerAddr  = "lower"
-)
-
-
+// =====================================================================================================================
 // SECTIONS:
 
 // (Tool bubbles get stored in separate modules that get imported.)
 
 // Model containers & relevant functions
 
-// Main model (bubbleboxer model)
 
 // Model container View, Update, and Init functions
 
 // bubbleboxer tree generator from config parsing
 
 // Model edit function
+
+// =====================================================================================================================
+
+// Main model (bubbleboxer model)
+
+type model struct {
+	layout boxer.Boxer
+}
+
+// =====================================================================================================================
+
+// Function to generate the bubbleboxer layout from the config
+
+// TODO: Add when tool bubbles made
+
+//func generateBoxerLayout(currentNode cfg.LayoutNode) () {
 //
+//}
+
+// =====================================================================================================================
 
 // Main function
 
-
-
 func main() {
-	// leaf content creation (models)
-	upper := spinnerHolder{spinner.New()}
-	left := stringer(leftAddr)
-	middle := stringer(middleAddr)
-	right := stringer(rightAddr)
-
-	lower := stringer(fmt.Sprintf("%s: use ctrl+c to quit", lowerAddr))
-
-	// layout-tree defintion
-	m := model{tui: boxer.Boxer{}}
-	m.tui.LayoutTree = boxer.Node{
-		// orientation
-		VerticalStacked: true,
-		// spacing
-		SizeFunc: func(_ boxer.Node, widthOrHeight int) []int {
-			return []int{
-				// since this node is vertical stacked return the height partioning since the width stays for all children fixed
-				widthOrHeight - 2,
-				1,
-				// make also sure that the amount of the returned ints match the amount of children:
-				// in this case two, but in more complex cases read the amount of the chilren from the len(boxer.Node.Children)
-			}
-		},
-		Children: []boxer.Node{
-			{
-				SizeFunc: func(_ boxer.Node, widthOrHeight int) []int {
-					return []int{
-						widthOrHeight/3,
-						(widthOrHeight/3)*2,
-
-					}
-				},
-				Children: []boxer.Node{
-					m.tui.CreateLeaf(upperAddr, upper),
-					{
-						VerticalStacked: true,
-						Children: []boxer.Node{
-							{
-								Children: []boxer.Node{
-									m.tui.CreateLeaf(leftAddr, left),
-									m.tui.CreateLeaf(rightAddr, right),
-								},
-							},
-							// make sure to encapsulate the models into a leaf with CreateLeaf:
-							m.tui.CreateLeaf(middleAddr, middle),
-						},
-					},
-				},
-			},
-			m.tui.CreateLeaf(lowerAddr, lower),
-
-		},
-	}
-	p := tea.NewProgram(m,tea.WithAltScreen())
-	if _, err := p.Run(); err != nil {
-		fmt.Println(err)
-	}
-}
-
-type model struct {
-	tui boxer.Boxer
-}
-
-func (m model) Init() tea.Cmd {
-	return spinner.Tick
-}
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.String() {
-		case "q", "ctrl+c":
-			return m, tea.Quit
-		}
-	case tea.WindowSizeMsg:
-		err := m.tui.UpdateSize(msg)
-		if err != nil {
-			return nil, nil
-		}
-	case spinner.TickMsg:
-		var cmd tea.Cmd
-		err := m.editModel(upperAddr, func(v tea.Model) (tea.Model, error) {
-			v, cmd = v.Update(msg)
-			return v, nil
-		})
-		if err != nil {
-			return nil, nil
-		}
-		return m, cmd
-	}
-	return m, nil
-}
-func (m model) View() string {
-	return m.tui.View()
-}
-
-func (m *model) editModel(addr string, edit func(tea.Model) (tea.Model, error)) error {
-	if edit == nil {
-		return fmt.Errorf("no edit function provided")
-	}
-	v, ok := m.tui.ModelMap[addr]
-	if !ok {
-		fmt.Errorf("no model with address '%s' found", addr)
-	}
-	v, err := edit(v)
+	// JSON to layout
+	jsonContent, err := os.ReadFile("defaultLayout.json")
+	err, schemaErr := cfg.LoadLayoutFile(string(jsonContent))
 	if err != nil {
-		return err
+		panic(err.Error())
+		os.Exit(1)
 	}
-	m.tui.ModelMap[addr] = v
-	return nil
-}
+	if len(schemaErr) > 0 {
+		fmt.Println("JSON Schema error:")
+		for errorCount, errorInfo := range schemaErr {
+			fmt.Println(strconv.Itoa(errorCount + 1) + " - " + errorInfo.Description())
+		}
+		os.Exit(1)
+	}
 
-type stringer string
+	// Load the config into a new bubbleboxer model.
 
-func (s stringer) String() string {
-	return string(s)
-}
 
-// satisfy the tea.Model interface
-func (s stringer) Init() tea.Cmd                           { return nil }
-func (s stringer) Update(msg tea.Msg) (tea.Model, tea.Cmd) { return s, nil }
-func (s stringer) View() string                            { return s.String() }
 
-type spinnerHolder struct {
-	m spinner.Model
-}
 
-func (s spinnerHolder) Init() tea.Cmd {
-	return s.m.Tick
-}
 
-func (s spinnerHolder) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	m, cmd := s.m.Update(msg)
-	s.m = m
-	return s, cmd
-}
-func (s spinnerHolder) View() string {
-	return s.m.View()
+
 }
